e79738abee6f6ce33c104693cbe3d3648e6c6cb3
diegolemaz <113269815+diegolemaz@users.noreply.github.com>
2023-10-04 11:19:10 -0300

    Netlify test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // Netlify test
+    },
   plugins: [react()],
 })
+
