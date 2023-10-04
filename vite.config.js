8bb8ba89e4c01881784cbdd6e3f05e2ae5903426
Diego Lema <dlz85@outlook.com>
2023-10-04 20:58:57 -0300

    Quitamos Netlify test

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // Netlify test
-    },
-  },
-  plugins: [react()],
+   plugins: [react()],
 })
 
